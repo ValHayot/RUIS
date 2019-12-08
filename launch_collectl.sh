c24e9d80b6067703337651389a9e35a101670f49
valhayot <valeriehayot@gmail.com>
2019-12-08 13:44:38 -0500

    alternative launch script

--- launch_collectl.sh
+++ launch_collectl.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+set -m
+
 free && sync && sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches" && free
-screen -S bids -dm bash -c "$2; screen -S collectl -X stuff $'\003'; exit"
-PID=$(ps aux | grep singularity | grep -v grep | awk -F ' ' '{print $2}')
-screen -S collectl -dm collectl $1 
+$2 &
+PID=$!
+echo "Application process ID: $PID"
+collectl -i:1 -sZ $1 --procfilt p$PID,P$PID &
+CCTL=$!
+echo "Collectl PID: $CCTL"
+JID=$(jobs -l | grep $PID | awk '{print $1;}' | sed 's/-//g' | sed 's/[][]//g')
+fg $JID
+kill $CCTL
+
+
 
